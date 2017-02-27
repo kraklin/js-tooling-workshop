aaed2d17c74becef0d68da6f6b76ba5f8cd25daf
Tomas Latal <latal.tomas@gmail.com>
2017-02-27 18:27:19 +0100

    Final version rewritten to classes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## Babel
+### Add babel to project
+`yarn add babel-cli babel-core babel-preset-env`
+
+### Run using CLI
+`./node_modules/.bin/babel ./src/js/app.js -o ./src/js/app.babel.js -w`
+
 ### Used resources
 #### Logo
 http://www.freeiconspng.com/free-images/volleyball-icon-3272
--- src/js/app.js
+++ src/js/app.js
@@ -1,64 +1,95 @@
+class Stats {
+    updateStats(statsData) {
+        var notDecided = statsData.filter((value) => value.isAttending === -1).length;
+        var areGoing = statsData.filter((value) => value.isAttending === 1).length;
+        var areNotGoing = statsData.filter((value) => value.isAttending === 0).length;
 
-let data = [
-    { id: 1, name: 'Jack', isAttending: -1 },
-    { id: 2, name: 'Jill', isAttending: -1 },
-    { id: 3, name: 'Steve', isAttending: -1 },
-    { id: 4, name: 'Peter', isAttending: -1 },
-    { id: 5, name: 'John', isAttending: -1 },
-    { id: 6, name: 'Kevin', isAttending: -1 }
-]
-
-const updateData = (id, attendance) => {
-    let toBeUpdated = data.filter((value) => value.id === id);
-
-    if (toBeUpdated.length !== 1)
-        return;
-
-    toBeUpdated[0].isAttending = attendance;
-    updateView(data);
-}
-
-function updateView(data) {
-    renderStats(data);
-    renderTable(data);
-}
-
-function renderStats(statsData) {
-    var notDecided = statsData.filter((value) => value.isAttending === -1).length;
-    var areGoing = statsData.filter((value) => value.isAttending === 1).length;
-    var areNotGoing = statsData.filter((value) => value.isAttending === 0).length;
-
-    $('#areNotDecided').html(notDecided);
-    $('#areGoing').html(areGoing);
-    $('#areNotGoing').html(areNotGoing);
-}
-
-function renderTable(data) {
-    $("#rooster tbody tr").remove();
-
-    for (var i = 0; i < data.length; i++) {
-        renderRow(data[i]);
+        $('#areNotDecided').html(notDecided);
+        $('#areGoing').html(areGoing);
+        $('#areNotGoing').html(areNotGoing);
     }
 }
 
-function renderRow(rowData) {
-    let row = $("<tr />");
-    row.append($(`<td>${rowData.name}</td>`));
-    row.append(renderThreeButtons(rowData));
-    $("#rooster").append(row);
+class Table {
+    constructor(tableId) {
+        this.tableId = tableId;
+    }
+
+    renderTable(tableData) {
+        this.clearRows();
+        for (var i = 0; i < tableData.length; i++) {
+            this.renderRow(tableData[i]);
+        }
+    }
+
+    clearRows(){
+        $(`${this.tableId} tbody tr`).remove();
+    }
+
+    renderRow(rowData) {
+        let row = $("<tr />");
+        row.append($(`<td>${rowData.name}</td>`));
+        row.append(this.renderThreeButtons(rowData));
+        $(this.tableId).append(row);
+    }
+
+    renderThreeButtons(rowData) {
+        return '<td class="center aligned"><div class="ui buttons">'
+            + this.renderButton(rowData.id, "Attend", rowData.isAttending, 1)
+            + this.renderButton(rowData.id, "Don't know", rowData.isAttending, -1)
+            + this.renderButton(rowData.id, "Not coming", rowData.isAttending, 0)
+            + '</div></td>';
+    }
+
+    renderButton(id, title, currentValue, setValue) {
+        var active = (currentValue === setValue) ? "active" : "";
+        return `<button class="ui ${active} button" onclick=\'updateData(${id},${setValue})\'>${title}</button>`;
+    }
 }
 
-function renderThreeButtons(rowData) {
-    return '<td class="center aligned"><div class="ui buttons">'
-        + renderButton(rowData.id, "Attend", rowData.isAttending, 1)
-        + renderButton(rowData.id, "Don't know", rowData.isAttending, -1)
-        + renderButton(rowData.id, "Not coming", rowData.isAttending, 0)
-        + '</div></td>';
+class Data {
+
+    constructor() {
+        this.data = [
+            { id: 1, name: 'Jack', isAttending: -1 },
+            { id: 2, name: 'Jill', isAttending: -1 },
+            { id: 3, name: 'Steve', isAttending: -1 },
+            { id: 4, name: 'Peter', isAttending: -1 },
+            { id: 5, name: 'John', isAttending: -1 },
+            { id: 6, name: 'Kevin', isAttending: -1 }
+        ]
+    }
+
+    load() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(this.data), 3000);
+        })
+    }
+
+    update(id, attendance) {
+        return new Promise((resolve, reject) => {
+            let toBeUpdated = this.data.filter((value) => value.id === id);
+
+            if (toBeUpdated.length !== 1)
+                reject();
+
+            toBeUpdated[0].isAttending = attendance;
+            resolve(this.data);
+        });
+    }
 }
 
-function renderButton(id, title, currentValue, setValue) {
-    var active = (currentValue === setValue) ? "active" : "";
-    return `<button class="ui ${active} button" onclick=\'updateData(${id},${setValue})\'>${title}</button>`;
+let stats = new Stats();
+let table = new Table("#rooster");
+let data = new Data();
+
+const updateData = (id, attendance) => {
+    data.update(id,attendance).then((data) => updateView(data));
 }
 
-updateView(data);
+const updateView = (data) => {
+    stats.updateStats(data);
+    table.renderTable(data);
+}
+
+data.load().then((data) => updateView(data))
