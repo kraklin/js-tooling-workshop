cc6410018494b23d4677746ae593924185fda3bb
Tomas Latal <latal.tomas@gmail.com>
2017-02-27 20:30:09 +0100

    added index.js as bootstrap for app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'script-loader!./js/jquery-3.1.1.min.js';
+import 'semantic-ui-css';
+
+// import 'semantic-ui-css';
+import 'semantic-ui-css/semantic.css';
+import css from './css/app.css';
+
+import * as App from './js/app.js';
+
+App.start();
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: {
         app: [
-            './src/js/app.js'
+            './src/index.js'
         ]
     },
 
@@ -13,7 +13,19 @@ module.exports = {
         rules: [{
             test: /\.js$/,
             use: ['babel-loader']    
-        }]
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },{
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      }
+        ]
     }
 }
 
